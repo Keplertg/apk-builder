3b924079d7feb841403cfaf693a0296c77f81b1c
Keplertg <lera5454896@gmail.com>
2026-06-20 10:25:20 +0600

    Add Appmqlut MainActivity [trigger build]

---
+++ apps/Appmqlut/app/src/main/java/com/botnexus/appmqlut/MainActivity.java
@@ -0,0 +1,96 @@
+package com.botnexus.appmqlut;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText num1EditText;
+    private EditText num2EditText;
+    private Button addButton;
+    private Button subtractButton;
+    private Button multiplyButton;
+    private Button divideButton;
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1EditText = findViewById(R.id.num1_edit_text);
+        num2EditText = findViewById(R.id.num2_edit_text);
+        addButton = findViewById(R.id.add_button);
+        subtractButton = findViewById(R.id.subtract_button);
+        multiplyButton = findViewById(R.id.multiply_button);
+        divideButton = findViewById(R.id.divide_button);
+        resultTextView = findViewById(R.id.result_text_view);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateAndDisplayResult("+");
+            }
+        });
+
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateAndDisplayResult("-");
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateAndDisplayResult("*");
+            }
+        });
+
+        divideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateAndDisplayResult("/");
+            }
+        });
+    }
+
+    private void calculateAndDisplayResult(String operation) {
+        try {
+            double num1 = Double.parseDouble(num1EditText.getText().toString());
+            double num2 = Double.parseDouble(num2EditText.getText().toString());
+            double result;
+
+            switch (operation) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        resultTextView.setText("Ошибка: деление на ноль");
+                        return;
+                    }
+                    break;
+                default:
+                    resultTextView.setText("Ошибка: неизвестная операция");
+                    return;
+            }
+
+            resultTextView.setText("Результат: " + result);
+        } catch (NumberFormatException e) {
+            resultTextView.setText("Ошибка: неверный формат ввода");
+        }
+    }
+}
